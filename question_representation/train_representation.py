072da234ceddadd2c924cbe665601badd37c3f8a
EUROPE\roy.sadaka <roys12>
2017-04-25 11:24:35 +0300

    *seperated model saving to "model" and "model_extra_epochs" *Added garbageCollecting to handle "AttributeError: 'NoneType' object has no attribute 'TF_DeleteStatus'"

--- question_representation/train_representation.py
+++ question_representation/train_representation.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 from keras.callbacks import ModelCheckpoint
+import gc
 
 import numpy as np
 import question_representation.model_creation_representation
@@ -16,34 +17,47 @@ BATCH_SIZE              = 256
 TRAIN_DATA_FILE         = "sample_train.csv"
 VALIDATION_SPLIT        = 0.2
 MODEL_FILE              = "model.h5"
+MODEL_FILE_EXTRA_EPOCHS = "model_extra_epochs.h5"
 
 def main():
     model, X_train, X_test, y_train, y_test = question_representation.model_creation_representation.main()
+
+    clear_old_files()
     train(model, X_train, y_train)
+    score(model, X_test, y_test)
+    gc.collect()
 
-    score = model.evaluate(np.array(X_test), np.array(y_test), verbose=0)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
 
+def clear_old_files():
+    print("[clear_old_files] - START")
+    try:
+        os.remove(MODEL_FILE)
+    except OSError:
+        pass
+
+    try:
+        os.remove(MODEL_FILE_EXTRA_EPOCHS)
+    except OSError:
+        pass
+    print("[clear_old_files] - END")
 
 def train(model, X_train, y_train):
     print("[train] - START")
 
     # validation_split - using TensorFlow recommendation, which is 1/12.
-    callbacks_save_best = [ModelCheckpoint("weights.h5", save_best_only=True, save_weights_only=False)]
+    callbacks_save_best = [ModelCheckpoint(MODEL_FILE, save_best_only=True, save_weights_only=False)]
     model.fit(np.array(X_train), np.array(y_train), validation_split = 1/12, epochs=10, batch_size=BATCH_SIZE, verbose=1 ,callbacks=callbacks_save_best)
 
 
-    callbacks = [ModelCheckpoint("weights.h5", save_best_only=False, save_weights_only=False)]
+    callbacks = [ModelCheckpoint(MODEL_FILE_EXTRA_EPOCHS, save_best_only=False, save_weights_only=False)]
     # run 3 more epochs using all training data (no validation_split)
     model.fit(np.array(X_train), np.array(y_train), epochs=3, batch_size=BATCH_SIZE, verbose=0 ,callbacks=callbacks)
-
-    try:
-        os.remove(MODEL_FILE)
-    except OSError:
-        pass
-    model.save(MODEL_FILE)
     print("[train] - END")
+
+def score(model, X_test, y_test):
+    score = model.evaluate(np.array(X_test), np.array(y_test), verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
 
 if __name__ == "__main__":
     main()
